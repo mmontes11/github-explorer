73a1a241c7404432fd77532b8eea9e368f800524
Martín Montes Pérez <martin.montes@nomasystems.com>
2019-07-03 21:26:04 +0200

    Getting organization

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import githubClient from "./githubClient";
 import './App.css';
 
-function App() {
+const App = () => {
+  const [orgInput, setOrgInput] = useState('');
+  const [orgResult, setOrgResult] = useState(null);
+  const [orgError, setOrgError] = useState(null); 
+  const onChange = ({ target: { value }}) => {
+    setOrgInput(value);
+  };
+  const onSubmit = async event => {
+    event.preventDefault();
+    try {
+      const { organization } = await githubClient.getOrganization(orgInput);
+      setOrgResult(organization);
+      setOrgError(null);
+    } catch (errors) {
+      setOrgError(new Error("Not found"));
+      setOrgResult(null);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <>
+      <h2>GitHub Explorer</h2>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Organization"
+          onChange={onChange}
+          value={orgInput}
+          className="search-input"
+        />
+        <button type="submit">
+          Search
+        </button>
+      </form>
+      {orgError &&
+        <h4 className="error">{orgError.message}</h4>
+      }
+      {orgResult &&
+        <a href={orgResult.url} target="blank">
+          <h4>
+            {orgResult.name}
+          </h4>
         </a>
-      </header>
-    </div>
+      }
+    </>
   );
 }
 
---
+++ src/githubClient.js
@@ -0,0 +1,44 @@
+class GitHubClient {
+  constructor() {
+    require('dotenv').config()
+    this.url = 'https://api.github.com/graphql';
+    this.authorization = `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`;
+  }
+  getOrganization(organization) {
+    const GET_ORGANIZATION = `
+      {
+        organization(login: ${organization}) {
+          name
+          url
+        }
+      }
+    `;
+    const body = {
+      query: GET_ORGANIZATION
+    };
+    return this._post(body);
+  }
+  async _post(body = {}) {
+    const req = await fetch(this.url, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': this.authorization
+      },
+      body: JSON.stringify(body)
+    });
+    const res = await req.json();
+    return new Promise((resolve, reject) => {
+      if (res.errors) {
+        reject(res.errors);
+      } else {
+        resolve(res.data);
+      }
+    });
+  }
+}
+
+const client = new GitHubClient();
+
+export default client
