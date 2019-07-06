25121bd39eb167e0244b95b7eb9faaa1f2802cdb
Martín Montes Pérez <martin.montes@nomasystems.com>
2019-07-06 13:05:04 +0200

    Importing dotenv config

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-
-require("dotenv").config();
+import 'dotenv/config';
 
 ReactDOM.render(<App />, document.getElementById("root"));
