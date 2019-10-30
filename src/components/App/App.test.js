565362ca83c44e9b2ff049649509b0617ed447f9
Martín Montes Pérez <martin.montes@nomasystems.com>
2019-10-30 18:35:16 +0100

    Fixed tests

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ApolloProvider } from "@apollo/react-hooks";
+import client from "../../graphql/apollo";
 import App from "./App";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>,
+    div,
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
