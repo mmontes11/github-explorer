293f04d9c8d219eeaf23dd4cfe3f8a1f4d653769
Martín Montes Pérez <martin.montes@nomasystems.com>
2019-11-08 16:32:52 +0100

    GITHUB_BASE_URL moved to constants

--- src/graphql/apollo.js
+++ src/graphql/apollo.js
@@ -1,8 +1,8 @@
 import { ApolloClient } from "apollo-client";
 import { HttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { GITHUB_BASE_URL } from "constants/index";
 
-const GITHUB_BASE_URL = "https://api.github.com/graphql";
 const authorization = `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`;
 const httpLink = new HttpLink({
   uri: GITHUB_BASE_URL,
