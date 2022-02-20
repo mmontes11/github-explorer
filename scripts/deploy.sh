4bd84df00bb73231f192a869d29acff706140010
Martín Montes <martin11lrx@gmail.com>
2022-02-20 21:36:12 +0100

    Updated deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,9 +2,10 @@
 
 RELEASE="github-explorer"
 REPO="mmontes"
+NAMESPACE="github-explorer"
 
 helm repo add "$REPO" https://charts.mmontes-dev.duckdns.org
 helm repo update
 
 echo "🚀 Deploying '${RELEASE}'..."
-helm upgrade --install "$RELEASE" "$REPO/$RELEASE"
+helm upgrade --install "$RELEASE" "$REPO/$RELEASE" --namespace $NAMESPACE
